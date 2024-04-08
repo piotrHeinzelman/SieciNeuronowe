1ef9245a93cf0e65cd690da2c1aaef757df0adc5
piotr <piotr@i-zn.pl>
2024-04-08 14:28:35 +0200

    Layer .

--- Projekt/Java/heinzelman/pl/project.java
+++ Projekt/Java/heinzelman/pl/project.java
@@ -16,6 +16,51 @@ class Pro{
     }
 
     public void run(){
+        // FORWARD layer 0:
+        Layer layer0 = new Layer( 2, 3, new FunSigMod() );
+        layer0.setIneuronWags(0, new Double[]{  1.0, -1.0 } );
+        layer0.setIneuronWags(1, new Double[]{  1.0,  1.0 } );
+        layer0.setIneuronWags(2, new Double[]{ -1.0,  1.0 } );
+
+        Double[] inputX = { 1.0, 2.0 };
+        layer0.setX( inputX );
+        layer0.calcU();
+        layer0.calcY();
+
+        Double[] layer0Y = layer0.getY(); // layer0Y = layet1X
+
+
+        // FORWARD layer 1:
+        Double[] layer1X = layer0Y;
+
+        Layer layer1 = new Layer( 3, 1, new FunSigMod() );
+        layer1.setIneuronWags(0, new Double[]{  1.0, -1.0,  1.0 } );
+
+        layer1.setX( layer1X );
+        layer1.calcU();
+        layer1.calcY();
+
+
+        // BACKPROPAGATION layer 1:
+        Double[] S_Y = layer1.calcDeltaError_S_Y( new Double[]{ 1.0 } ); // OCZEKIWANA
+        layer1.calcD( S_Y );
+        layer1.calcAndUpdateW();
+        layer1.calcS();
+        Double[] D_for_layer0 = layer1.getS_is_dForPrevoiusLayer();
+        //System.out.println( layer1 );
+
+        // BACKPROPAGATION layer 0:
+        layer0.calcD( D_for_layer0 );
+        layer0.calcAndUpdateW();
+        layer0.calcS();
+        Double[] D_for_EXIT = layer0.getS_is_dForPrevoiusLayer();
+
+        System.out.println( "Error on exit:" + D_for_EXIT[0] );
+        System.out.println( layer0 );
+        System.out.println( layer1 );
+    }
+
+    public void runOFF(){
         System.out.println( "start" );
 
         // FORWARD layer 0:
