ed655637452aa937dcef1c8b50bdfd0c33370538
Bat <bat@man.pl>
2024-04-14 22:06:30 +0200

    update Neuron & Layer

--- Projekt/Java/heinzelman/pl/neurons/Teacher.java
+++ Projekt/Java/heinzelman/pl/neurons/Teacher.java
@@ -2,8 +2,6 @@ package heinzelman.pl.neurons;
 
 public class Teacher {
 
-    public Double[] updateSfromTeacher( Double[] Z ){
-        return new Double[]{ 1.0-Z[0], 0.0-Z[1] };
-    }
+    public Double[] updateSfromTeacher( Double[] Z ){ return new Double[]{ 1.0-Z[0], 0.0-Z[1] };  }
 
 }
